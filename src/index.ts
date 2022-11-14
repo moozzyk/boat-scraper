3bed34d5f37f22f39dfc0ad57e6363b4dc54d4af
moozzyk <moozzyk@gmail.com>
2022-11-13 21:41:53 -0800

    Dry run

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+import { forSaleCarAndTrucks } from "craigslist-automation";
+
+(async () => {
+  let value = 0;
+  for await (let galleryPost of forSaleCarAndTrucks("seattle", {
+    query: "blazer k5",
+  })) {
+    console.log(galleryPost);
+    const post = await galleryPost.getPost();
+    console.log(post);
+    if (value++ > 3) {
+      break;
+    }
+  }
+})();
