1e2d981976a4d4dcfe282d899a14835a74b44a21
moozzyk <moozzyk@gmail.com>
2022-11-19 20:30:01 -0800

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 import { Database } from "sqlite3-async";
 import { DB_FILENAME } from "./common";
+import express from "express";
+
+const port: number = 3000;
+
+const app: express.Application = express();
+
+app.get("/", (req, res) => {
+  res.send("TypeScript With Express");
+});
+
+app.listen(port, () => {
+  console.log(`TypeScript with Express
+       http://localhost:${port}/`);
+});
