38d905f0855a17a81e2d5f4c71089721db5686e9
moozzyk <moozzyk@gmail.com>
2022-11-19 20:08:10 -0800

    Remove unneeded table

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,6 @@ async function createDatabase(): Promise<void> {
   await db.open();
   await db.run(`
   CREATE TABLE IF NOT EXISTS Posts(
-    url TEXT PRIMARY KEY,
-    title TEXT,
-    date_posted TIMESTAMP NOT NULL,
-    date_updated TIMESTAMP,
-    ts TIMESTAMP);`);
-  await db.run(`
-  CREATE TABLE IF NOT EXISTS PostVersions(
     url TEXT NOT NULL,
     date TIMESTAMP NOT NULL,
     title TEXT,
@@ -24,6 +17,7 @@ async function createDatabase(): Promise<void> {
     attributes TEXT,
     images TEXT,
     location TEXT,
+    ts TIMESTAMP,
     PRIMARY KEY (url, date),
     FOREIGN KEY (url) REFERENCES Posts(url));`);
   await db.close();
@@ -33,9 +27,8 @@ async function createDatabase(): Promise<void> {
   await createDatabase();
   let db = new Database(DB_FILENAME);
   await db.open();
-  let insertPost = await db.prepare("INSERT INTO Posts VALUES(?, ?, ?, ?, ?);");
-  let insertPostVersion = await db.prepare(
-    "INSERT INTO PostVersions VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);"
+  let insertPost = await db.prepare(
+    "INSERT INTO Posts VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
   );
   let value = 0;
   for await (let galleryPost of forSaleCarsAndTrucks("seattle", {
@@ -46,13 +39,6 @@ async function createDatabase(): Promise<void> {
     console.log(post);
     await insertPost.run(
       post.url,
-      post.title,
-      post.datePosted,
-      post.dateUpdated ?? null,
-      Date.now()
-    );
-    await insertPostVersion.run(
-      post.url,
       post.dateUpdated ?? post.datePosted,
       post.title,
       post.price,
@@ -60,13 +46,13 @@ async function createDatabase(): Promise<void> {
       post.description,
       post.attributes ? JSON.stringify(post.attributes) : "NULL",
       post.images ? JSON.stringify(post.images) : "NULL",
-      [post.city, post.state].join(", ")
+      [post.city, post.state].join(", "),
+      Date.now()
     );
     if (value++ > 3) {
       break;
     }
   }
   await insertPost.finalize();
-  await insertPostVersion.finalize();
   await db.close();
 })();
