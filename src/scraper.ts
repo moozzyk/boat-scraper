08b8aa39352198b961c4c7f446b6714da297caa0
moozzyk <moozzyk@gmail.com>
2022-11-20 00:27:06 -0800

    Save boats

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,4 +1,5 @@
-import { forSaleCarsAndTrucks } from "craigslist-automation";
+import { forSaleBoats } from "craigslist-automation";
+import { GalleryPost } from "craigslist-automation/types";
 import { Database } from "sqlite3-async";
 import { DB_FILENAME } from "./common";
 
@@ -22,6 +23,14 @@ async function createDatabase(): Promise<void> {
   await db.close();
 }
 
+const regex = new RegExp(
+  "^19\\d\\d|seadoo|sea-doo|whaler|sailboat|vintage|smokecraft|pontoon|dingy|inflatable|superjet",
+  "i"
+);
+function shouldSkip(post: GalleryPost): boolean {
+  return regex.test(post.title || "");
+}
+
 (async () => {
   await createDatabase();
   let db = new Database(DB_FILENAME);
@@ -29,11 +38,15 @@ async function createDatabase(): Promise<void> {
   let insertPost = await db.prepare(
     "INSERT INTO Posts VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
   );
-  let value = 0;
-  for await (let galleryPost of forSaleCarsAndTrucks("seattle", {
-    query: "blazer k5",
+  for await (let galleryPost of forSaleBoats("seattle", {
+    hasImage: true,
+    minPrice: 5000,
+    maxPrice: 35000,
   })) {
     console.log(galleryPost);
+    if (shouldSkip(galleryPost)) {
+      continue;
+    }
     const post = await galleryPost.getPost();
     console.log(post);
     await insertPost.run(
@@ -48,9 +61,6 @@ async function createDatabase(): Promise<void> {
       [post.city, post.state].join(", "),
       Date.now()
     );
-    if (value++ > 3) {
-      break;
-    }
   }
   await insertPost.finalize();
   await db.close();
