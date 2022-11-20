f3d1d4e72cfc41ce1be60f57fb0371b088f34e71
moozzyk <moozzyk@gmail.com>
2022-11-19 19:31:16 -0800

    Populate posts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,42 @@
 import { forSaleCarsAndTrucks } from "craigslist-automation";
+import { Database } from "sqlite3-async";
+
+const DB_FILENAME = "/Users/moozzyk/tmp/testdb";
+
+async function createDatabase(): Promise<void> {
+  let db = new Database(DB_FILENAME);
+  await db.open();
+  await db.run(`
+  CREATE TABLE IF NOT EXISTS Posts(
+    url TEXT PRIMARY KEY,
+    title TEXT,
+    date_posted TIMESTAMP NOT NULL,
+    date_updated TIMESTAMP,
+    ts TIMESTAMP);`);
+  await db.run(`
+  CREATE TABLE IF NOT EXISTS PostVersions(
+    url TEXT NOT NULL,
+    date TIMESTAMP NOT NULL,
+    title TEXT,
+    price INTEGER,
+    currency TEXT,
+    description TEXT,
+    attributes TEXT,
+    images TEXT,
+    location TEXT,
+    PRIMARY KEY (url, date),
+    FOREIGN KEY (url) REFERENCES Posts(url));`);
+  await db.close();
+}
 
 (async () => {
+  await createDatabase();
+  let db = new Database(DB_FILENAME);
+  await db.open();
+  let insertPost = await db.prepare("INSERT INTO Posts VALUES(?, ?, ?, ?, ?);");
+  let insertPostVersion = await db.prepare(
+    "INSERT INTO PostVersions VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);"
+  );
   let value = 0;
   for await (let galleryPost of forSaleCarsAndTrucks("seattle", {
     query: "blazer k5",
@@ -8,8 +44,29 @@ import { forSaleCarsAndTrucks } from "craigslist-automation";
     console.log(galleryPost);
     const post = await galleryPost.getPost();
     console.log(post);
+    await insertPost.run(
+      post.url,
+      post.title,
+      post.datePosted,
+      post.dateUpdated ?? null,
+      Date.now()
+    );
+    await insertPostVersion.run(
+      post.url,
+      post.dateUpdated ?? post.datePosted,
+      post.title,
+      post.price,
+      post.currency,
+      post.description,
+      post.attributes ? JSON.stringify(post.attributes) : "NULL",
+      post.images ? JSON.stringify(post.images) : "NULL",
+      [post.city, post.state].join(", ")
+    );
     if (value++ > 3) {
       break;
     }
   }
+  await insertPost.finalize();
+  await insertPostVersion.finalize();
+  await db.close();
 })();
