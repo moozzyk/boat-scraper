973f1a459cb9e3ad5a9e3fe536869836483ed1e7
moozzyk <moozzyk@gmail.com>
2023-02-05 20:39:35 -0800

    Group posts by url or title

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,28 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+function groupPosts(posts: any[]): Map<string, any[]> {
+  const groupedPosts = new Map<string, any[]>();
+  const titleToUrlMap = new Map<string, string>();
+  posts.forEach((post) => {
+    if (groupedPosts.has(post.url)) {
+      groupedPosts.get(post.url)?.push(post);
+    } else {
+      if (titleToUrlMap.has(post.title)) {
+        const url = titleToUrlMap.get(post.title)!;
+        groupedPosts.get(url)?.push(post);
+      } else {
+        groupedPosts.set(post.url, [post]);
+        titleToUrlMap.set(post.title, post.url);
+      }
+    }
+  });
+  return groupedPosts;
+}
+
 app.get("/", (req, res) => {
-  const posts = new PostsDb(getDatabasePath()).getPosts();
-  res.render("index", { posts });
+  const posts = groupPosts(new PostsDb(getDatabasePath()).getPosts());
+  res.render("index", { posts: posts.values() });
 });
 
 app.get("/hide", (req, res) => {
